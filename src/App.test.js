24e8d5c3ca6c6d003fc9a4fcee5c45e8bcb44940
Cristian Lesmes <119712186+lezmez@users.noreply.github.com>
2023-11-24 17:21:34 -0500

    ajustes API, app.test.js y otras mas

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,38 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import {Catalog} from './components/Catalog';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// Prueba 1: Verificar la presencia del título "Catálogo" ===========================
+test('renders app with correct title', () => {
+  render(<Catalog />);
+  const titleElement = screen.getByText(/Catalog/i);
+  expect(titleElement).toBeInTheDocument();
+});
+
+// Prueba 2: Verificar la presencia de al menos un producto en el catálogo ==========
+test('renders at least one product in the catalog', () => {
+  render(<Catalog></Catalog>);
+  const productElement = screen.getByText(/Add to Cart/i); 
+  expect(productElement).toBeInTheDocument();
+});
+
+// Prueba 3: Verificar si un botón específico está presente =========================
+test('renders a specific button in the app', () => {
+  render(<Catalog></Catalog>);
+  const specificButton = screen.getByRole('button', { name: /Add to Card/i }); 
+  expect(specificButton).toBeInTheDocument();
+});
+
+// Prueba 4: Verificar que se rendericen todas las imágenes de los productos =========
+test('renders all product images in the catalog', () => {
+  render(<Catalog></Catalog>);
+  const productImages = screen.getAllByRole('img');
+  expect(productImages).toHaveLength(5);
+});
+
+// Prueba 5: Verificar el texto del botón de un producto específico ==================
+test('verifies text of a specific product button', () => {
+  render(<Catalog></Catalog>);
+  const specificProductButton = screen.getByRole('button', { name: /Add to cart/i });
+  expect(specificProductButton).toHaveTextContent('Add to cart');
 });
--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -1,27 +1,54 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import './Weather.css';
+import axios from 'axios';
 
 const Weather = () => {
   const [weatherData, setWeatherData] = useState(null);
   const [error, setError] = useState(null);
-  const API_KEY = '488af23542bdfd55774b77748c11ceee'; 
-  const CITY = 'San gil, Santander, Colombia';
+  const API_KEY = '488af23542bdfd55774b77748c11ceee';
+  const DEFAULT_CITY = 'San gil, Santander, Colombia';
+
+  const fetchWeatherData = async (location) => {
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}&units=metric`
+      );
+      setWeatherData(response.data);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  const fetchLocalWeather = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        async (position) => {
+          const { latitude, longitude } = position.coords;
+          try {
+            const response = await axios.get(
+              `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+            );
+            setWeatherData(response.data);
+          } catch (error) {
+            setError(error.message);
+          }
+        },
+        (error) => {
+          setError(error.message);
+        }
+      );
+    } else {
+      setError('Geolocation is not supported by this browser.');
+    }
+  };
+
+  const fetchDefaultWeather = () => {
+    fetchWeatherData(DEFAULT_CITY);
+  };
 
   useEffect(() => {
-    const fetchWeatherData = async () => {
-      try {
-        const response = await axios.get(
-          `https://api.openweathermap.org/data/2.5/weather?q=${CITY}&appid=${API_KEY}&units=metric`
-        );
-        setWeatherData(response.data);
-      } catch (error) {
-        setError(error.message);
-      }
-    };
-
-    fetchWeatherData();
-  }, [API_KEY, CITY]);
+    fetchWeatherData(DEFAULT_CITY);
+  }, [API_KEY, DEFAULT_CITY]);
 
   if (error) {
     return <div>Error: {error}</div>;
@@ -33,10 +60,13 @@ const Weather = () => {
 
   return (
     <div className="weather">
-      <h2>Weather in 📌{CITY} 🗺️</h2>
+      <h2>Weather in 📌{weatherData.name}, {weatherData.sys.country} 🗺️</h2>
       <p>Temperature: {weatherData.main.temp}°C</p>
       <p>Weather: {weatherData.weather[0].main}</p>
       <p2>☀️🌤️🌦️🌧️⛈️🌩️🌨️🌪️🌘🌙</p2>
+
+      <button onClick={fetchDefaultWeather}>Default Weather</button>
+      <button onClick={fetchLocalWeather}>Local Weather</button>
     </div>
   );
 };
